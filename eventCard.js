e7d0d37e8e72c5dc2221b59a55d7a9aa9799aab5
Sandra Spighel <s.spighel@gmail.com>
2021-03-10 14:08:17 +0000

    Create eventIndex component

--- eventCard.js
+++ eventCard.js
@@ -1,6 +1,13 @@
+
 class EventCard extends HTMLElement {
   constructor() {
     super()
+
+    // // Create shadow DOM
+    // this.attachShadow({ mode: 'open' })
+    // this.shadowRoot.appendChild()
+
+
     this.innerHTML = 'Testing event card'
   }
 }
---
+++ eventIndex.js
@@ -0,0 +1,7 @@
+function init() {
+
+  console.log('DOM Loaded!')
+
+}
+
+window.addEventListener('DOMContentLoaded', init)
