bf9efcaacb8bc34c238700b90c419988d12ff895
Sandra Spighel <s.spighel@gmail.com>
2021-03-10 15:04:07 +0000

    Fetch data and log it in the console

--- eventIndex.js
+++ eventIndex.js
@@ -1,6 +1,28 @@
+//todo   Wait for the DOM to load before fetching the data
 function init() {
 
+  //* confirm that the DOM has loaded
   console.log('DOM Loaded!')
+
+  //* declare an empty array to store the upcomingLaunchesData objects
+  let upcomingLaunchesData = []
+
+  // * fetching the data and storing it in the upcomingLaunchesData array
+  async function getData() {
+
+    try {
+      const response = await window.fetch('https://api.spacexdata.com/v4/launches/upcoming')
+      upcomingLaunchesData = await response.json()
+      console.log('Upcoming Launches : ', upcomingLaunchesData)
+
+    } catch (err) {
+      console.log('An error occured : ', err)
+    }
+  }
+
+
+
+  getData()
 
 }
 
